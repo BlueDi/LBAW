a5a33143cbd85bbdaeed49b192901b7ad41df533
Diogo Cruz <diogohmcruz@hotmail.com>
2017-02-25 15:21:55 +0000

    Popup Register/Login

--- js/login.js
+++ js/login.js
@@ -1,3 +1,18 @@
-$('#toggle-login').click(function(){
-  $('#login').toggle();
+$('#toggle-login_popup').click(function(){
+  $('#login_popup').toggle();
+});
+
+$('.tab a').on('click', function (e) {
+
+  e.preventDefault();
+
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+
+  $(target).fadeIn(600);
+
 });
