54c8b3fd10caa977622f5ac5e76071d5dc526d71
killghost15 <killghost15@gmail.com>
2017-04-28 22:25:56 +0100

    functions of database required for indexpage listing

--- PIU/database/products.php
+++ PIU/database/products.php
@@ -15,6 +15,28 @@ function selectProductByCategory($idcategory,$number){
     global $conn;
      $stmt = $conn->prepare("SELECT * FROM product WHERE idcategory=? LIMIT ?");
      $stmt->execute(array($idcategory,$number));
+     return $stmt->fetchAll();
 }
 
-     ?>
+function selectNewProducts($number){
+    global $conn;
+     $stmt = $conn->prepare("SELECT * FROM product JOIN category ON product.idcategory=category.idcategory AND category.name='NEW' LIMIT ?");
+     $stmt->execute(array($number));
+     return $stmt->fetchAll();
+}
+function ChangeStock($id,$value){
+     global $conn;
+     $stmt = $conn->prepare("UPDATE product SET stock=? WHERE idproduct= ? ");
+     $stmt->execute(array($idproduct,$value));
+    
+}
+
+function getImageUrlFromProduct($id)
+{    
+     global $conn;
+     $stmt = $conn->prepare("SELECT url FROM image JOIN product ON product.idimage=image.idimage AND product.idproduct=? LIMIT 1");
+     $stmt->execute(array($id));
+     return $stmt->fetch();
+}
+
+?>
