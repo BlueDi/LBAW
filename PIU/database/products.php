7894a21e936c58be6de2922f6a22e76c5d7ed406
Diogo Cruz <diogohmcruz@hotmail.com>
2017-05-06 18:09:25 +0100

    Reencaminhamento dos Produtos da mesma categoria no single.html
    Meti os produtos da mesma categoria a dar no single.html. A maneira como
    metemos código php ainda temos que combinar porque dá para por de muitas
    maneiras e depois nao fica coeso. Não sei qual a melhor ou qual a mais
    organizada. Também limpei o comentário do fim do single.html.
    Organizei o products.php. É boa prática nao fechar a tag <?php num
    ficheiro só de php.
    Limpei do flexslider.css.

--- PIU/database/products.php
+++ PIU/database/products.php
@@ -1,47 +1,47 @@
 <?php
-
-
-
-function CreateProduct($imgid,$productname,$price,$stock,$weight,$width,$height,$description,$categoryid,$brandid){
-    
-global $conn;
+function CreateProduct($imgid, $productname, $price, $stock, $weight, $width, $height, $description, $categoryid, $brandid)
+{
+    global $conn;
     $stmt = $conn->prepare("INSERT INTO product VALUES (DEFAULT, ?, ?,?,?,?,?,?,?,?,?)");
-    $stmt->execute(array($productname, $imgid,$categoryid,$brandid,$price,$stock,$weight,$width,$height,$description));
-
+    $stmt->execute(array($productname, $imgid, $categoryid, $brandid, $price, $stock, $weight, $width, $height, $description));
 }
 
-
-function selectProductByCategory($idcategory,$number){
+function selectProductByCategory($idcategory, $number)
+{
     global $conn;
-     $stmt = $conn->prepare("SELECT * FROM product WHERE idcategory=? LIMIT ?");
-     $stmt->execute(array($idcategory,$number));
-     return $stmt->fetchAll();
+    $stmt = $conn->prepare("SELECT * FROM product WHERE idcategory=? LIMIT ?");
+    $stmt->execute(array($idcategory, $number));
+    return $stmt->fetchAll();
 }
 
-function selectNewProducts($number){
+function selectNewProducts($number)
+{
     global $conn;
-     $stmt = $conn->prepare("SELECT product.idproduct,product.name,product.idimage,product.price,product.stock FROM product JOIN category ON product.idcategory=category.idcategory AND category.name='NEW' LIMIT ?");
-     $stmt->execute(array($number));
-     return $stmt->fetchAll();
+    $stmt = $conn->prepare("SELECT product.idproduct,product.name,product.idimage,product.price,product.stock FROM product JOIN category ON product.idcategory=category.idcategory AND category.name='NEW' LIMIT ?");
+    $stmt->execute(array($number));
+    return $stmt->fetchAll();
 }
-function ChangeStock($id,$value){
-     global $conn;
-     $stmt = $conn->prepare("UPDATE product SET stock=? WHERE idproduct= ? ");
-     $stmt->execute(array($idproduct,$value));
-    
+
+function ChangeStock($id, $value)
+{
+    global $conn;
+    $stmt = $conn->prepare("UPDATE product SET stock=? WHERE idproduct= ? ");
+    $stmt->execute(array($id, $value));
 }
 
 function getImageUrlFromProduct($id)
-{    
-     global $conn;
-     $stmt = $conn->prepare("SELECT url FROM image JOIN product ON product.idimage=image.idimage AND product.idproduct=? LIMIT 1");
-     $stmt->execute(array($id));
-     return $stmt->fetch();
-}
-function getProductById($id){
+{
     global $conn;
-     $stmt = $conn->prepare("SELECT * FROM product WHERE idproduct= ?");
-     $stmt->execute(array($id));
-     return $stmt->fetch();
+    $stmt = $conn->prepare("SELECT url FROM image JOIN product ON product.idimage=image.idimage AND product.idproduct=? LIMIT 1");
+    $stmt->execute(array($id));
+    return $stmt->fetch();
 }
-?>
+
+function getProductById($id)
+{
+    global $conn;
+    $stmt = $conn->prepare("SELECT * FROM product WHERE idproduct= ?");
+    $stmt->execute(array($id));
+    return $stmt->fetch();
+}
+
