c850363a58a61750ae7f80b16be887bab5810b87
Diogo Cruz <diogohmcruz@hotmail.com>
2017-05-08 16:42:30 +0100

    Redesign do Product Creation
    Segui a mesma estrutura do form de login/register.
    Também se podia mudar o texto da imagem.
    Talvez o form pudesse ser mais largo.

--- PIU/actions/products/Insert.php
+++ PIU/actions/products/Insert.php
@@ -1,24 +1,29 @@
 <?php
 include_once('../../config/init.php');
-include($BASE_DIR.'/database/clients.php');
+include($BASE_DIR . 'database/clients.php');
+include('../images/upload.php');
 
-include('../images/upload.php');			
+$imgid = GetImageId($_FILES["fileToUpload"]["name"]);
+$productname = $_POST['ProductName'];
+$price = $_POST['Price'];
+$stock = $_POST['Stock'];
+$weight = $_POST['Weight'];
+$width = $_POST['Width'];
+$height = $_POST['Height'];
+$description = $_POST['Description'];
+$categoryid = $_POST['category'];
+$brandid = $_POST['brand'];
 
+CreateProduct($imgid, $productname, $price, $stock, $weight, $width, $height, $description, $categoryid, $brandid);
+echo '
+    <script>
+        alert("Product created");
+    </script>
+';
 
-$imgid=GetImageId($_FILES["fileToUpload"]["name"]);
-$productname=$_POST['ProductName'];
-$price=$_POST['Price'];
-$stock=$_POST['Stock'];
-$weight=$_POST['Weight'];
-$width=$_POST['Width'];
-$height=$_POST['Height'];
-$description=$_POST['Description'];
-$categoryid=$_POST['category'];
-$brandid=$_POST['brand'];
-            CreateProduct($imgid,$productname,$price,$stock,$weight,$width,$height,$description,$categoryid,$brandid);
-            echo'<script language="javascript">';
-				echo 'alert("Product created")';
-                echo '</script>';
-                $redirectUrl = '../../pages/products.php';
-				echo '<script type="application/javascript">window.location.href = "'.$redirectUrl.'";</script>';
-?>
+$redirectUrl = '../../pages/products.php';
+echo '
+    <script>
+        window.location.href = "' . $redirectUrl . '";
+    </script>
+';
