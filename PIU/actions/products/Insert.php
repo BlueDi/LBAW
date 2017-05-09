fd21f165d7730c82c31177d2b415e7cdc87e26f1
killghost15 <killghost15@gmail.com>
2017-05-09 04:41:52 +0100

    wishlist need to fix the situation of unadding and already added exception

--- PIU/actions/products/Insert.php
+++ PIU/actions/products/Insert.php
@@ -1,29 +1,24 @@
 <?php
 include_once('../../config/init.php');
-include($BASE_DIR . 'database/clients.php');
-include('../images/upload.php');
+include($BASE_DIR.'/database/clients.php');
 
-$imgid = GetImageId($_FILES["fileToUpload"]["name"]);
-$productname = $_POST['ProductName'];
-$price = $_POST['Price'];
-$stock = $_POST['Stock'];
-$weight = $_POST['Weight'];
-$width = $_POST['Width'];
-$height = $_POST['Height'];
-$description = $_POST['Description'];
-$categoryid = $_POST['category'];
-$brandid = $_POST['brand'];
+include('../images/upload.php');			
 
-CreateProduct($imgid, $productname, $price, $stock, $weight, $width, $height, $description, $categoryid, $brandid);
-echo '
-    <script>
-        alert("Product created");
-    </script>
-';
 
-$redirectUrl = '../../pages/products.php';
-echo '
-    <script>
-        window.location.href = "' . $redirectUrl . '";
-    </script>
-';
+$imgid=GetImageId($_FILES["fileToUpload"]["name"]);
+$productname=$_POST['ProductName'];
+$price=$_POST['Price'];
+$stock=$_POST['Stock'];
+$weight=$_POST['Weight'];
+$width=$_POST['Width'];
+$height=$_POST['Height'];
+$description=$_POST['Description'];
+$categoryid=$_POST['category'];
+$brandid=$_POST['brand'];
+            CreateProduct($imgid,$productname,$price,$stock,$weight,$width,$height,$description,$categoryid,$brandid);
+            echo'<script language="javascript">';
+				echo 'alert("Product created")';
+                echo '</script>';
+                $redirectUrl = '../../pages/products.php';
+				echo '<script type="application/javascript">window.location.href = "'.$redirectUrl.'";</script>';
+?>
---
+++ PIU/actions/wishlist/add.php
@@ -0,0 +1,18 @@
+<?php
+  include_once('../../config/init.php');
+  include_once($BASE_DIR .'database/wishlist.php'); 
+
+$productid = strip_tags($_POST['Idproduct']);
+$userid = strip_tags($_POST['Iduser']);
+
+
+if(CreateWhishlistEntry($userid,$productid)==true){
+                $redirectUrl = $BASE_PIU.'pages/profile.php';
+				echo '<script type="application/javascript">window.location.href = "'.$redirectUrl.'";</script>';
+    
+}
+else{
+    
+}
+
+?>
